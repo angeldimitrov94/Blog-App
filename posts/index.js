39b0c5a52767e7d404f80bc70649f1eee5b1a9dd
Angel Dimitrov <angel.dimitrov@omron.com>
2021-02-07 11:47:37 -0800

    Add updated route for creating new posts '/posts/create/'

--- posts/index.js
+++ posts/index.js
@@ -16,7 +16,7 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
 
@@ -36,6 +36,6 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(postsPort, () => {
-  console.log('v55');
+  console.log('v1000');
   console.log(`"Posts" service listening on port ${postsPort}`);
 });
